578f049ef815ee5254b433733de4c56a83174f8e
aguchaves <aguchaves@gmail.com>
2016-04-28 21:20:39 -0300

    Update topic 2 (points 6,7,8)

--- 02-oop-inheritance/js/jscript.js
+++ 02-oop-inheritance/js/jscript.js
@@ -19,11 +19,12 @@ class EventEmitter {
 }
 
 class Movie extends EventEmitter {
-    constructor (title, year, duration){
+    constructor (title, year, duration, cast){
         super()
         this.title = title
         this.year = year
         this.duration = duration
+        this.cast = []
 }
     play(){
         this.emit("play")
@@ -34,6 +35,17 @@ class Movie extends EventEmitter {
     resume(){
         this.emit("resume")
     }
+    addCast(actors){
+    let thisActor = this.cast
+        if (Array.isArray(actors)){
+            actors.forEach(function(eachActor){
+                thisActor.push(eachActor)
+            })
+        } else {
+            thisActor.push(actors)
+        }
+        
+    }
 }
 
 class Logger {
@@ -43,8 +55,35 @@ class Logger {
     }
 }
 
+let Social = {
+    share: function(friendName){console.log(`Share ${this.title} with   ${friendName}`)},
+    like: function(friendName){console.log(`${friendName} like  ${this.title} `)},
+}
+
+class Actor {
+    constructor (name, lastName, age){
+        this.name = name
+        this.lastName = lastName
+        this.age = age
+    }
+}
+
 let theGodfather = new Movie("The Godfather", 1972, "2h58m");
+let marlon = new Actor("Marlon","Brando",80);
+let otherCastGF = [ new Actor("Al","Pacino",75),
+                  new Actor("Diane","Keaton",72),
+                  new Actor("Robert","Duvall",84)
+];
+
+let pulp = new Movie("Pulp Fiction", 1994, "2h34m");
+let samuel = new Actor("Samuel","Jackson",68);
+let otherCastPF = [ new Actor("John","Travolta",62),
+                  new Actor("Bruce","Willis",61),
+                  new Actor("Uma","Thurman",46)
+];
+
 let logger = new Logger();
+let copia = Object.assign(theGodfather, Social);
 
 
 theGodfather.on("play", logger.log);
@@ -53,4 +92,15 @@ theGodfather.on("resume", logger.log);
 theGodfather.emit('play');
 theGodfather.emit('pause');
 theGodfather.emit('resume');
+
+copia.share('Dante');
+copia.like('Dante');
+
+theGodfather.addCast(marlon);
+theGodfather.addCast(otherCastGF);
+console.log(theGodfather);
+
+pulp.addCast(samuel);
+pulp.addCast(otherCastPF);
+console.log(pulp);
 
